a8bd76fb2951cca6876fa242f20d5789c1ffe6f4
Benoit <benoit-aliaga@orange.fr>
2020-10-07 21:43:22 +0200

    version finale

--- Systeme/Python/TP_01/bonjour.py
+++ Systeme/Python/TP_01/bonjour.py
@@ -6,6 +6,7 @@ import sys
 pyScriptName = sys.argv[0]
 firstParam = sys.argv[1]
 factParam = sys.argv[2]
+nbrPrem = sys.argv[3]
 
 #if len(sys.argv) == 1:
 #    print("Vous devez donner un parametre au script.")
@@ -37,11 +38,21 @@ def fact(x):
 print(fact(int(factParam)))    
 
 print("\n")
+print("Nombres Premiers \n")
 
 # n premiers entiers positifs
 
+print("{} est-il un nombre premier ?".format(nbrPrem))
+
 def premier(x):
-    if x == 0 and x == 1:
-        print("c'est un nombre premier")
+    if x == 0 or x == 1:
+        print("C'est un nombre premier")
     else:
+        assert x >= 2
+        for i in range(2, x):
+            if x%i  == 0:
+                return "Ce n'est pas un nombre premier."
+        return "C'est un nombre premier"
+
+print(premier(int(nbrPrem)))
         
