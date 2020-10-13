b310e7839fbdc6755a1192cee5647a6694abb9d8
Benoit <benoit-aliaga@orange.fr>
2020-10-13 12:54:27 +0200

    Quizz fonctionnel mais reste a ajouter le nombre de bonne reponse

--- Systeme/Python/TP_02/TP_02.py
+++ Systeme/Python/TP_02/TP_02.py
@@ -16,34 +16,32 @@ import sys, re, random
 # System parameters
 pyScriptName = sys.argv[0]
 fileName = sys.argv[1]
-outputFilePays = sys.argv[2]
-outputFileCapital = sys.argv[3]
 
 # Variables
 pays = []
 capitales = []
 
 # Read a file
-print(fileName)
-with open(fileName, "r") as capital_file, open(outputFilePays, "w") as output_pays, open(outputFileCapital, "w") as output_capital:
+with open(fileName, "r") as capital_file:
     for line in capital_file:
-        test = line.rsplit(",")
-        #pays = test[0]
+        test = line.rstrip().rsplit(",")
         pays.append(test[0])
-        #output_pays.write(pays + "\n")
-        #capital = test[1]
         capitales.append(test[1])
-        #output_capital.write(capital)
+        
+# Choose randomly a country
+alea = random.randint(1,len(pays)-1)
 
-#print(pays)
-#print(capitales)
+# Ask a question to the player
+print("\n Welcome to the Super Quizz! \n")
+print("Do you know the capital of:", pays[alea] + "\n")
 
-# Save the list into a txt file
-capital_file.close()
-output_pays.close()
-output_capital.close()
+# Player answer:
+answer = input("Your answer:")
 
-# Print randomly a country
-print(len(pays))
-alea = random.randint(1,len(pays))
-print(pays[alea])
+# Test the player answer
+if answer == capitales[alea]:
+    print("Good response!")
+else:
+    print("Wrong. Try again")
+    print(capitales[alea])
+
