a352367b5bc43632857ddb1b37f73db64aa21485
Benoit <benoit-aliaga@orange.fr>
2020-10-19 23:15:17 +0200

    Ajout nbre et percentage bonne reponse

--- Systeme/Python/TP_02/TP_02.py
+++ Systeme/Python/TP_02/TP_02.py
@@ -9,39 +9,59 @@
 #                        #
 ##########################
 
+
+""" 
+    Quizz game on countries and theirs capitals  
+"""
+
 # Import modules
 import sys, re, random
-
 
 # System parameters
 pyScriptName = sys.argv[0]
 fileName = sys.argv[1]
+question = sys.argv[2]
 
 # Variables
 pays = []
 capitales = []
+attempt = 1
+responseOK = 0 # count the good answers
 
 # Read a file
 with open(fileName, "r") as capital_file:
     for line in capital_file:
-        test = line.rstrip().rsplit(",")
+        test = line.rstrip().rsplit(",") # remove the 
         pays.append(test[0])
         capitales.append(test[1])
-        
-# Choose randomly a country
-alea = random.randint(1,len(pays)-1)
+
+
+### MAIN
 
 # Ask a question to the player
 print("\n Welcome to the Super Quizz! \n")
-print("Do you know the capital of:", pays[alea] + "\n")
 
-# Player answer:
-answer = input("Your answer:")
+print("You have " , question , " question(s). \n")
 
-# Test the player answer
-if answer == capitales[alea]:
-    print("Good response!")
-else:
-    print("Wrong. Try again")
-    print(capitales[alea])
+while attempt <= int(question):
+    # Choose randomly a country
+    alea = random.randint(1,len(pays)-1)
+    
+    print("Do you know the capital of:", pays[alea] + "\n")
 
+    # Player answer:
+    answer = input("Your answer:")
+
+    # Test the player answer
+    if answer == capitales[alea]:
+        print("\n Good response!")
+        responseOK += 1
+        percentage = (responseOK / attempt) * 100
+        print("You have {} good answers ({:.2f}%).".format(responseOK, percentage))
+    else:
+        print("Wrong. Try again! \n")
+        print(capitales[alea])
+        percentage = (responseOK / attempt) * 100
+        print("You have {} good answers ({:.2f}%).".format(responseOK, percentage))
+
+    attempt += 1
