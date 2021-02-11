cc9a1ac9016bba02567396cf5977072a35b727e2
Benoit <benoit-aliaga@orange.fr>
2021-02-11 23:46:54 +0100

    function processFastaFile

--- cpp/fasta.cpp
+++ cpp/fasta.cpp
@@ -4,30 +4,24 @@
 
 using namespace std;
 
-ifstream myFlux("calmodulin.fasta");  //Ouverture d'un fichier en lecture
+ifstream myFlux("calmodulin.fasta");
 
+void processFastaFile(){
+  if(myFlux) {
+    string line;
+    while(getline(myFlux, line)) {
+         cout << line << endl;
+    }
+  }
+  else {
+      cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
+  }
+}
 
 int main() {
+  processFastaFile();
   
-  if(myFlux)
-    {
-      //Tout est prêt pour la lecture.
-      string line;
-      while(getline(myFlux, line)) //Tant qu'on n'est pas à la fin, on lit
-      {
-         cout << line << endl;
-         //Et on l'affiche dans la console
-         //Ou alors on fait quelque chose avec cette ligne
-         //À vous de voir
-      }
-    }
-  else
-    {
-      cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
-    }
-
-  return 0;
-  
+  return 0;  
 }
 
 
